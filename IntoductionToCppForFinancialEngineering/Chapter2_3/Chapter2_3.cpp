a4a45f3f9d3fa87b2186fa6b8d54c73763c6ae6f
Tanvir Singh <tanvirsethi7@gmail.com>
2023-01-17 01:35:47 -0500

    new commit with a few changes

--- IntoductionToCppForFinancialEngineering/Chapter2_3/Chapter2_3.cpp
+++ IntoductionToCppForFinancialEngineering/Chapter2_3/Chapter2_3.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 	int x = 10;
 	int y = 20;
+	bool winningLotteryTicket = false;
+	string porc = "CALL";
 
 	cout << "Before if statement, x = " << x << "; y = " << y << endl;
 	if (x == 10) {
@@ -15,6 +18,27 @@ int main() {
 	}
 
 	cout << "After exiting if statement, x = " << x << "; y = " << y << endl;
+
+
+	if (winningLotteryTicket) {
+		cout << "You can now retire in Aruba!" << endl;
+	}
+	else {
+		cout << "Sorry Pal, back to work for you!" << endl;
+	}
+
+	if (porc == "PUT") {
+		cout << "You have a put option." << endl;
+	}
+	else if (porc == "CALL") {
+		cout << "You have a call option." << endl;
+	}
+	else {
+		cout << "You have a problem." << endl;
+	}
+
+
+	cout << endl;
 
 	return 0;
 }
